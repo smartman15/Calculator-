8ecd866e672ec4f9b04f87fabea8687b9c13e17e
khayri <khayriramzy@gmail.com>
2025-02-28 11:34:50 -0300

    Create variables to store two numbers and an operator Create function operate() to call a function based on operator

--- javascript.js
+++ javascript.js
@@ -13,3 +13,25 @@ function multiply(a, b){
 function divide(a, b){
     return a / b;
 }
+
+function operate(num1, num2, operator){
+    switch(operator){
+        case '+':
+            add(num1, num2);
+            break;
+        case '-':
+            substract(num1, num2);
+            break;
+        case '*':
+            multiply(num1, num2);
+            break;
+        case '/':
+            divide(num1, num2);
+            break;
+        default:
+            console.log('error');
+    }
+}
+
+
+let num1, num2, operator;
